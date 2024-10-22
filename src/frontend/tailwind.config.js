f05b6a484a1d76cbc9307f4bf0c4d0c9798faf1d
Luis Gilles <luisgillesc@gmail.com>
2024-10-21 22:34:04 -0300

    first commit

---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Incluye todos los archivos de tu proyecto React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
